b164b777988be8b3b79b81e393b02110d90a2b8a
Nigel Eke <nigeleke@gmail.com>
2023-02-10 22:32:08 +1100

    Add explicit baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  "app": {
+    "baseURL": "/nigeleke.github.io/jacquialexander.com.au/",
+  },
+
   css: [
     '@/assets/css/main.scss'
   ],
