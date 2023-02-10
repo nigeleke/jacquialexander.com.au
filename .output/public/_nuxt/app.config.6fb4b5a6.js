423f401a841bc0dd4f16d513527dd0b91c39a23e
Nigel Eke <nigeleke@users.noreply.github.com>
2023-02-10 12:03:38 +0000

    Release

---
+++ .output/public/_nuxt/app.config.6fb4b5a6.js
@@ -0,0 +1,1 @@
+import{B as i,C as n}from"./entry.5946daa0.js";import"./nuxt-link.95e408eb.js";const e=i({title:"Jacqui Alexander Singing Studio",theme:{dark:!0,colors:{primary:"#ff0000"}}}),o={};n(e,o);
