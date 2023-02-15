a2f0300906178cf58d8dd5732bb5421ace320cbb
Nigel Eke <nigeleke@gmail.com>
2023-02-16 08:31:27 +1100

    Rebuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  runtimeConfig: {
-    public: {
-      baseURL: 'http://jacquialexander.com.au/', // Exposed to the frontend as well.
-      cdnURL: 'http://jacquialexander.com.au/', // Exposed to the frontend as well.
-    }
-  },
-
   css: [
     '@/assets/css/main.scss'
   ],
