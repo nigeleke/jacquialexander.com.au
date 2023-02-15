13219812b00ebc461a7b635eb00aa937c78bbd61
Nigel Eke <nigeleke@gmail.com>
2023-02-16 10:07:56 +1100

    Publish

---
+++ docs/_nuxt/app.config.579e6a15.js
@@ -0,0 +1,1 @@
+import{B as i,C as n}from"./entry.036f5f2e.js";import"./nuxt-link.69d6dd77.js";const e=i({title:"Jacqui Alexander Singing Studio",theme:{dark:!0,colors:{primary:"#ff0000"}}}),o={};n(e,o);
---
+++ docs/_nuxt/composables.d892c5d9.js
@@ -0,0 +1,1 @@
+import{l as r}from"./entry.036f5f2e.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
